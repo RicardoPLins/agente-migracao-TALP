f1e884a24f1b63829fcd51b56786655c06845a08
Ricardo Lins <ricardinho200215@gmail.com>
2026-05-15 12:12:35 -0300

    add: code example to migrate

--- url.py
+++ url.py
@@ -1,2 +1,12 @@
-import urllib
-import urllib2
+ def create_user(self, user_info):
+        try:
+            return self.client.post_json('users/', user_info)
+        except urllib2.HTTPError, err:
+            if err.code == 400:
+                raise self.BadParams()
+            if err.code == 402:
+                raise self.PaymentRequired()
+            elif err.code == 409:
+                data = json.loads(err.read())
+                if 'username' in data['conflicts']:
+                    raise self.DuplicateUsername()
