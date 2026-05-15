6d4da453a1de0b83763e5800a421ddad84b05391
Ricardo Lins <ricardinho200215@gmail.com>
2026-05-15 12:23:28 -0300

    add: code before migration

--- url.py
+++ url.py
@@ -1,13 +1,12 @@
- try:
-            resp = self.client.post_json_raw_response(
-                'groups/', group_info)
+ def create_user(self, user_info):
+        try:
+            return self.client.post_json('users/', user_info)
         except urllib2.HTTPError, err:
             if err.code == 400:
                 raise self.BadParams()
+            if err.code == 402:
+                raise self.PaymentRequired()
             elif err.code == 409:
                 data = json.loads(err.read())
-                if 'name' in data['conflicts']:
-                    raise self.DuplicateGroupName()
-                elif 'plan_id' in data['conflicts']:
-                    raise self.BadPlan()
-            raise
+                if 'username' in data['conflicts']:
+                    raise self.DuplicateUsername()
